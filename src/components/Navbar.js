d5054785218684236f04ade85c50a0992d98889e
Moldir <moldirbaijanova03@gmail.com>
2024-01-18 22:35:08 +0600

    1

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react'
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='navbar-container'>
+            <p>cvbnm,</p>
+        </div>
+    </nav>
+
+  )
+}
+
+export default Navbar
